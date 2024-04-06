fd8564ec89140e6ab696454de4fc0c9d3a78facf
Akihiko Odaki <akihiko.odaki@daynix.com>
2024-04-06 15:23:02 +0900

    Make $dnf array

--- create.sh
+++ create.sh
@@ -5,7 +5,7 @@ set -eu
 src="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 . "$src/var/rc"
 bind_src="$(sed -e 's/\\\"/""/g' <<< "$src/var")"
-podman run --rm "--mount=type=bind,src=$bind_src,dst=/host,relabel=private" -i -- "$image" sudo bash -es -- "$dnf" <<'EOF'
+podman run --rm "--mount=type=bind,src=$bind_src,dst=/host,relabel=private" -i -- "$image" sudo bash -es -- "${dnf[@]}" <<'EOF'
 rm -fr /host/root
 dnf install --installroot=/host/root --setopt=cachedir=/host/cache --setopt=keepcache=1 -y toolbox-support "$@"
 EOF
